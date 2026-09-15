f3bd4177edd8d1470b1207c8f4e4f28455ccd9df
AhmedAliElashmawy <trophie.hunter.gamer@gmail.com>
2026-09-15 20:13:08 +0300

    feat(I2C): Finish I2C driver and verify its functionality
    I2C_SendRepeatedStart doesn't work in SimulIDE v1.1 due to a simulator bug (returns 0x08 instead of 0x10). Works correctly on real hardware.

--- MCAL/I2C/I2C_private.h
+++ MCAL/I2C/I2C_private.h
@@ -39,5 +39,39 @@
  */
 
 /* TODO: map TWBR, TWSR, TWDR, TWCR and the bit names. */
+#ifndef F_CPU
+#define F_CPU 8000000UL
+#endif
+
+#define TWBR_REG  (*(volatile uint8*)0x20)
+#define TWSR_REG  (*(volatile uint8*)0x21)
+#define TWAR_REG  (*(volatile uint8*)0x22)
+#define TWDR_REG  (*(volatile uint8*)0x23)
+#define TWCR_REG  (*(volatile uint8*)0x56)
+
+#define TWINT 7
+#define TWEA  6
+#define TWSTA 5
+#define TWSTO 4
+#define TWEN  2
+
+#define TWSR_STATUS_MASK    0xF8u
+#define TWSR_PRESCALER_MASK 0x03u
+
+#define I2C_TIMEOUT         50000UL
+
+#define MASTER_STEP(TWCR_EXTRA, EXPECTED_STATUS) \
+    do { \
+        uint32 Local_u32Timeout = I2C_TIMEOUT; \
+        TWCR_REG = (1u << TWINT) | (1u << TWEN) | (TWCR_EXTRA); \
+        while (((TWCR_REG & (1u << TWINT)) == 0) && (--Local_u32Timeout > 0)) ; \
+        if (Local_u32Timeout == 0) return E_NOK; \
+        if ((TWSR_REG & TWSR_STATUS_MASK) != (EXPECTED_STATUS)) return E_NOK; \
+    } while(0)
+
+#define SLA_W(address) (((address) << 1) | 0)
+#define SLA_R(address) (((address) << 1) | 1)
+
+#define TWBR_VALUE(F_CPU, SCL) (((F_CPU) / (SCL) - 16) / 2)
 
 #endif /* I2C_PRIVATE_H */
